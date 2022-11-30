750adf9b9f2e9ac7d6e3e7a4b28f3592dcad3390
Vidya <vidyajaywant2000@gmail.com>
2022-11-30 11:25:51 +0530

    Add Carousel

--- src/sections/CompaniesWeHelpedComponent.js
+++ src/sections/CompaniesWeHelpedComponent.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Carousel, Container } from "react-bootstrap";
 import SectionTitle from "../Commons/SectionTitle";
 import { getPostedJobFilterList } from "../config/api";
 
@@ -18,7 +18,35 @@ export default function CompaniesWeHelpedComponent() {
   //       alert(error);
   //     });
   // }
-  const companyname = ['/images/company/lexschool.jpeg', '/images/company/lexcare.jpeg', '/images/company/dhage-associates.jpeg', '/images/company/jusoculus.jpeg', '/images/company/ms-associates.jpeg', '/images/company/piyushchobe.jpeg'];
+
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  // const companyname = [
+  //   "/images/company/lexschool.jpeg",
+  //   "/images/company/lexcare.jpeg",
+  //   "/images/company/dhage-associates.jpeg",
+  //   "/images/company/jusoculus.jpeg",
+  //   "/images/company/ms-associates.jpeg",
+  //   "/images/company/piyushchobe.jpeg",
+  // ];
+  const comnamefirst = [
+    "/images/company/lexschool.jpeg",
+    "/images/company/lexcare.jpeg",
+    "/images/company/dhage-associates.jpeg",
+    "/images/company/jusoculus.jpeg",
+    "/images/company/ms-associates.jpeg",
+  ];
+  const comnamesecond = [
+    "/images/company/piyushchobe.jpeg",
+    "/images/company/lexschool.jpeg",
+    "/images/company/lexcare.jpeg",
+    "/images/company/dhage-associates.jpeg",
+    "/images/company/jusoculus.jpeg",
+  ];
   return (
     <>
       <section className="app__section border-bottom">
@@ -28,14 +56,39 @@ export default function CompaniesWeHelpedComponent() {
             subTitle="Some of the companies we've helped recruit excellent applicants over the years."
           />
           <div className="app__compWeHelpd">
-            {companyname.map((item, index) =>
+            {/* {companyname.map((item, index) => (
               <div className="app__comp-img" key={index}>
                 <a href={"/jobs?cn="}>
                   <img src={item} alt="" style={{ maxHeight: 80 }} />
                 </a>
               </div>
-            )}
+            ))} */}
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+              <div className="app__compWeHelpd">
+                {comnamefirst.map((item, index) => (
+                  <div className="app__comp-img" key={index}>
+                    <a href={"/jobs?cn="}>
+                      <img src={item} alt="" style={{ maxHeight: 80 }} />
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </Carousel.Item>
+            <Carousel.Item>
+              <div className="app__compWeHelpd">
+                {comnamesecond.map((item, index) => (
+                  <div className="app__comp-img" key={index}>
+                    <a href={"/jobs?cn="}>
+                      <img src={item} alt="" style={{ maxHeight: 80 }} />
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </Carousel.Item>
+          </Carousel>
           </div>
+          
         </Container>
       </section>
     </>
